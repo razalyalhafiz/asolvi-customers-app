2d6bca50c654abc94fbaab6a816af762c3d78e73
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-12 18:30:27 +0800

    added config

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+
+const config = require("./config.json");
+const PORT = process.env.PORT || config.PORT;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/public/views/");
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
     res.render('index');
 })
 
-app.listen(port);
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
