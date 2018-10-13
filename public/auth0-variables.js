0e3f82765a5d63f69097a41a80ad01b2325e15c2
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-14 04:17:22 +0800

    test

--- public/auth0-variables.js
+++ public/auth0-variables.js
@@ -2,4 +2,4 @@ var AUTH0_CLIENT_ID = "OPEBMU2ClhWLFYo7JWDXzS0S3GzM9Oax";
 var AUTH0_DOMAIN = "razalyalhafiz.au.auth0.com";
 var AUTH0_CALLBACK_URL = "https://asolvi-customers-app.herokuapp.com";
 var AUTH0_CALLBACK_URL_ENCODED = "https%3A%2F%2Fasolvi%2Dcustomers%2Dapp.herokuapp.com";
-var API_AUDIENCE = "https://asolvi-customers-api.herokuapp.com"
+var API_AUDIENCE = "https://asolvi-customers-api.herokuapp.com/customers"
