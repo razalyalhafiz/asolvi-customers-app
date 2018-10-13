101d5a66dd8e1aa9c406ffdb2d8a3410fea8a59a
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-13 12:52:27 +0800

    mod

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.set("views", __dirname + "/public/views/");
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
-    res.render('index');
-    res.render("customers", {
+    res.render('index', {
       customers: []
     });
 })
