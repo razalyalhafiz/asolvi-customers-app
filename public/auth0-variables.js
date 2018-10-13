caad78c7ecb74ab59d94ba6c397762cd2dd2f6f7
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-13 16:42:32 +0800

    test

--- public/auth0-variables.js
+++ public/auth0-variables.js
@@ -1,3 +1,3 @@
 var AUTH0_CLIENT_ID = "OPEBMU2ClhWLFYo7JWDXzS0S3GzM9Oax";
 var AUTH0_DOMAIN = "razalyalhafiz.au.auth0.com";
-var AUTH0_CALLBACK_URL = "https://asolvi-customers-app.herokuapp.com";
+var AUTH0_CALLBACK_URL = "https%3A%2F%2Fasolvi-customers-app.herokuapp.com"
