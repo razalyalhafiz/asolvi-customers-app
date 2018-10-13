761552b69f74a3109d691d7b9e1fac5c2ef1d95c
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-13 15:46:06 +0800

    test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const config = require("./config.json");
+const PORT = process.env.PORT || config.PORT;
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/public/views/");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+      customers: []
+    });
+})
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
