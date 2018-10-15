53245fbe3c2bf9ecd9e0b92f3ca3690e88fcaed0
Razaly Alhafiz <razalyalhafiz@MacBook-Pro.lan>
2018-10-15 17:23:16 +0800

    test

--- public/app.js
+++ public/app.js
@@ -1,14 +1,18 @@
-window.addEventListener('load', function() {
-  var content = document.querySelector('.content');
-  var loadingPage = document.getElementById("loading_page");
-  var loadingDiv = document.getElementById("loading_div")
-  var alert = document.getElementById("alert");
-  var alertHeading = document.querySelector(".alert-heading");
-  var alertMessage = document.querySelector(".alert-msg");
+window.addEventListener("load", function() {
+  var content = document.querySelector(".content")
+  var loadingPage = document.getElementById("loadingPage")
+  var loadingDiv = document.getElementById("loadingDiv")
+  var alert = document.getElementById("alert")
+  var alertHeading = document.querySelector(".alert-heading")
+  var alertMessage = document.querySelector(".alert-msg")
+  var loginStatus = document.querySelector(".main-view h4")
+  var message = document.querySelector(".main-view p")
+  var loginBtn = document.getElementById("btnLogin")
+  var logoutBtn = document.getElementById("btnLogout")
 
-  content.style.display = 'block';
-  loadingPage.style.display = "none";
-  alert.style.display = "none";
+  content.style.display = "block"
+  loadingPage.style.display = "none"
+  alert.style.display = "none"
 
   var webAuth = new window.auth0.WebAuth({
     domain: AUTH0_DOMAIN,
@@ -19,42 +23,40 @@ window.addEventListener('load', function() {
     scope: "openid profile"
   })
 
-  var loginStatus = document.querySelector(".container h4");
-  var message = document.querySelector('.container p');
+  loginBtn.addEventListener("click", function(e) {
+    // Initiate Auth0 login
+    e.preventDefault()
+    webAuth.authorize()
+  })
 
-  // buttons and event listeners
-  var loginBtn = document.getElementById('qsLoginBtn');
-  var logoutBtn = document.getElementById('qsLogoutBtn');
-
-  loginBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    webAuth.authorize();
-  });
-
-  logoutBtn.addEventListener('click', logout);
+  logoutBtn.addEventListener("click", logout)
 
   function setSession(authResult) {
     // Set the time that the access token will expire at
     var expiresAt = JSON.stringify(
       authResult.expiresIn * 1000 + new Date().getTime()
-    );
+    )
 
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
+    // Store entries from authResult into localStorage
+    localStorage.setItem("access_token", authResult.accessToken)
+    localStorage.setItem("id_token", authResult.idToken)
+    localStorage.setItem("expires_at", expiresAt)
   }
 
   function getCustomers(accessToken) {
-    if (!accessToken)
-      return;
-    console.log(`access_token: ${accessToken}`);
+    if (!accessToken) return
+    console.log(`access_token: ${accessToken}`)
 
-    fetch(`${API_AUDIENCE}/customers`, { headers: { Authorization: "Bearer " + accessToken } })
+    // Fetch the customers data from the API
+    fetch(`${API_AUDIENCE}/customers`, {
+      headers: { Authorization: "Bearer " + accessToken }
+    })
       .then(res => res.json())
       .then(response => {
-        console.log('Success:', JSON.stringify(response));
-        loadingDiv.style.display = "none";
+        console.log("Success:", JSON.stringify(response))
+        loadingDiv.style.display = "none"
 
+        // Display data in jQuery DataTable
         $("#tblCustomers").DataTable({
           data: response,
           paging: false,
@@ -65,12 +67,12 @@ window.addEventListener('load', function() {
             { data: "name", title: "Name" },
             { data: "position", title: "Position" },
             { data: "company", title: "Company" },
-            { data: "status", title: "Status" }   
+            { data: "status", title: "Status" }
           ],
           columnDefs: [
             {
               targets: 1,
-              render: function (data) {
+              render: function(data) {
                 return '<img class="avatar" src="' + data + '">'
               }
             }
@@ -80,73 +82,87 @@ window.addEventListener('load', function() {
             {
               text: "All",
               action: function(e, dt, node, config) {
-                dt.search('').columns(5).search('').draw();
+                dt.search("")
+                  .columns(5)
+                  .search("")
+                  .draw()
               }
             },
             {
               text: "Hot",
-              action: function (e, dt, node, config) {
-                dt.columns(5).search('Hot').draw();
+              action: function(e, dt, node, config) {
+                dt.columns(5)
+                  .search("Hot")
+                  .draw()
               }
             },
-            {   
+            {
               text: "Cool",
-              action: function (e, dt, node, config) {
-                dt.columns(5).search('Cool').draw();
+              action: function(e, dt, node, config) {
+                dt.columns(5)
+                  .search("Cool")
+                  .draw()
               }
             }
           ]
         })
       })
-      .catch(error => console.error('Error:', error));
+      .catch(error => console.error("Error:", error))
   }
 
   function logout() {
     // Remove tokens and expiry time from localStorage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
+    localStorage.removeItem("access_token")
+    localStorage.removeItem("id_token")
+    localStorage.removeItem("expires_at")
 
-    location.href = `https://${AUTH0_DOMAIN}/v2/logout?returnTo=${AUTH0_CALLBACK_URL_ENCODED}`;
+    // Initiate Auth0 logout
+    location.href = `https://${AUTH0_DOMAIN}/v2/logout?returnTo=${AUTH0_CALLBACK_URL_ENCODED}`
   }
 
   function isAuthenticated() {
-    // Check whether the current time is past the
-    // access token's expiry time
-    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
+    // Check whether the current time is past the access token's expiry time
+    var expiresAt = JSON.parse(localStorage.getItem("expires_at"))
+    return new Date().getTime() < expiresAt
   }
 
   function handleAuthentication() {
-    loginBtn.style.display = 'inline-block';
-    logoutBtn.style.display = 'none';
-    loginStatus.innerHTML = 'You are not logged in! Please log in to continue.';
-
+    // Initiate Auth0 hash parsing
     webAuth.parseHash(function(err, authResult) {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        console.log('Auth Result: ' + JSON.stringify(authResult));
-        setSession(authResult);
-        getCustomers(authResult.accessToken);
-        toggleView(true, "You are granted the <i>sales</i> role, thus you may view the list of customers below.");
+        console.log("Auth Result: " + JSON.stringify(authResult))
+        setSession(authResult)
+        getCustomers(authResult.accessToken)
+        toggleView(
+          true,
+          "You are granted the <i>sales</i> role, thus you may view the list of customers below."
+        )
       } else if (isAuthenticated()) {
-        let accessToken = localStorage.getItem('access_token');
-        getCustomers(accessToken);
-        toggleView(true, "You are granted the <i>sales</i> role, thus you may view the list of customers below.");
+        let accessToken = localStorage.getItem("access_token")
+        getCustomers(accessToken)
+        toggleView(
+          true,
+          "You are granted the <i>sales</i> role, thus you may view the list of customers below."
+        )
       } else if (err) {
-        console.log(err);       
+        console.log(err)
         loadingDiv.style.display = "none"
-        toggleView(true, "You are granted the <i>general</i> role, thus you are prohibited to view the list of customers.");
-        alert.style.display = "block";
-        alertHeading.innerHTML = `Error: ${err.error}`;
-        alertMessage.innerHTML = err.errorDescription;
+        toggleView(
+          true,
+          "You are granted the <i>general</i> role, thus you are prohibited to view the list of customers."
+        )
+        alert.style.display = "block"
+        alertHeading.innerHTML = `Error: ${err.error}`
+        alertMessage.innerHTML = err.errorDescription
       } else {
         loadingDiv.style.display = "none"
-        toggleView(false);
+        toggleView(false)
       }
-    });
+    })
   }
 
   function toggleView(isLoggedIn, messageText) {
+    // Toggle UI controls based on login status
     if (isLoggedIn) {
       loginBtn.style.display = "none"
       logoutBtn.style.display = "inline-block"
@@ -155,9 +171,10 @@ window.addEventListener('load', function() {
     } else {
       loginBtn.style.display = "inline-block"
       logoutBtn.style.display = "none"
-      loginStatus.innerHTML = "You are not logged in! Please log in to continue."
+      loginStatus.innerHTML =
+        "You are not logged in! Please log in to continue."
     }
   }
 
-  handleAuthentication();
-});
+  handleAuthentication()
+})
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/public/views/");
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/public/views/")
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"))
 
-app.get('/', function (req, res) {
-    res.render('index', {
-      customers: []
-    });
+app.get("/", function(req, res) {
+  res.render("index", {
+    customers: []
+  })
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
